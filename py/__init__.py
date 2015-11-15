071c2f1a39ebe7a6949c47813a6a8d21745f03cd
Sam Lerman <slerman@u.rochester.edu>
2015-11-15 15:21:57 -0500

    Added test message to server

--- py/__init__.py
+++ py/__init__.py
@@ -25,7 +25,7 @@ def createJob():
                                              coordinates['N'], coordinates['S'], keywords, "rules")
 
     createJob = CreateHistoricalJob.post(username, password, timeframe['fromDate'], timeframe['toDate'], title, rules)
-    return json.dumps({'status': 'OK', 'createJob': createJob})
+    return json.dumps({'status': 'OK', 'message': 'Success', 'createJob': createJob})
 
 
 @app.route('/acceptRejectJob', methods=['POST'])
